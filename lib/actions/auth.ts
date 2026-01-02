7254be1d662f0b9d9bca1301ef076482116e2750
salah-abukhatwa <roben.gn2@gmail.com>
2026-01-02 09:50:01 +0300

    fix resend form

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -67,7 +67,7 @@ export const signUp = async (params: AuthCredentials) => {
 
     // ✅ IMPORTANT: this URL must be PUBLIC in production (Vercel domain)
     await workflowClient.trigger({
-      url: `https://library-inky-five.vercel.app/api/workflow`,
+      url: `${config.env.appUrl}/api/workflow`,
 
       body: { email, fullName },
     });
